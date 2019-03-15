ab6ee9c3b90fee95e1ffb4f9d320a9d1c567f2b6
jian.sun <jian.sun@vipshop.com>
2019-03-15 16:15:22 +0800

    +resource static config

--- src/main/java/com/aihuishou/bi/md/WebMvcConfiguration.java
+++ src/main/java/com/aihuishou/bi/md/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package com.aihuishou.bi.md;
 import com.aihuishou.bi.md.front.auth.CurrentUserResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.annotation.Resource;
@@ -16,5 +17,10 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Override
     protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(currentUserResolver);
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/");
     }
 }
