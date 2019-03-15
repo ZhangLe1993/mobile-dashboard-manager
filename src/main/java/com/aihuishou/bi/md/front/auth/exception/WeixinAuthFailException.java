1d260c6d79c712db05943097847471b8ee9c2f93
jian.sun <jian.sun@vipshop.com>
2019-03-15 18:07:12 +0800

    微信校验失败4006

--- src/main/java/com/aihuishou/bi/md/front/auth/exception/WeixinAuthFailException.java
+++ src/main/java/com/aihuishou/bi/md/front/auth/exception/WeixinAuthFailException.java
@@ -3,6 +3,6 @@ package com.aihuishou.bi.md.front.auth.exception;
 public class WeixinAuthFailException extends AuthException  {
 
     public WeixinAuthFailException() {
-        super(4005, "微信Server校验失败");
+        super(4006, "微信Server校验失败");
     }
 }
