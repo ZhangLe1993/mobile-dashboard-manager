efdd9fe5b8a61489ba5ddc4f5356e75266c7e245
jian.sun <jian.sun@vipshop.com>
2019-03-19 13:55:32 +0800

    summary接口格式变更

--- src/main/java/com/aihuishou/bi/md/front/chart/gmv/GmvC.java
+++ src/main/java/com/aihuishou/bi/md/front/chart/gmv/GmvC.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.sql.Date;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,9 +26,14 @@ public class GmvC {
     @RequestMapping("/summary")
     public ResponseEntity summary() {
         Date lastDataDate = gmvService.getLastDataDate();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(lastDataDate);
+        double monthProgress = (double) cal.get(Calendar.DAY_OF_MONTH) / cal.getMaximum(Calendar.DAY_OF_MONTH);
+        DecimalFormat nf=new DecimalFormat("00.00%");
         List<SummaryBean> summary = gmvService.querySummary();
         Map<String, Object> result = new HashMap();
         result.put("date", new SimpleDateFormat("yyyy-MM-dd").format(lastDataDate));
+        result.put("date_progress",nf.format(monthProgress));
         result.put("data", summary);
         return new ResponseEntity(result, HttpStatus.OK);
     }
