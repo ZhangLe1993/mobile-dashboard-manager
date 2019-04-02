df55b772757e64b1fdf9c58bbd087eabaf024e92
jian.sun <jian.sun@vipshop.com>
2019-04-02 10:22:48 +0800

    only clear md /form_ids?open-id=xxx

--- src/main/java/com/aihuishou/bi/md/back/HomeC.java
+++ src/main/java/com/aihuishou/bi/md/back/HomeC.java
@@ -1,10 +1,10 @@
 package com.aihuishou.bi.md.back;
 
+import com.aihuishou.bi.md.front.cache.CacheHolder;
 import com.aihuishou.bi.md.front.chart.gmv.GmvDataDateService;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
+import com.aihuishou.bi.md.front.notice.FormId;
+import com.aihuishou.bi.md.front.notice.SendMessJob;
+import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,34 +12,38 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 @RestController
 @RequestMapping("/back")
 public class HomeC {
 
     @Resource
-    private RedisTemplate redisTemplate;
+    private SendMessJob sendMessJob;
 
     @Resource
     private GmvDataDateService gmvDataDateService;
 
+    @Resource
+    private CacheManager cacheManager;
+
     @RequestMapping("/clear_md")
-    public void clearCache() {
-        redisTemplate.execute(new RedisCallback() {
-            @Override
-            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
-                redisConnection.flushDb();//TODO 只clear md
-                return null;
-            }
-        });
+    public void clearMd() {
+        cacheManager.getCache(CacheHolder.CACHE_NAME).clear();
     }
 
     @RequestMapping("/gmv_data_date")
     public String updateGmvDataDate(@RequestParam(value = "date", required = false) String date) throws ParseException {
-        clearCache();
+        clearMd();
         gmvDataDateService.setLastDataDate(date);
         return new SimpleDateFormat("yyyy-MM-dd").format(gmvDataDateService.getLastDataDate());
     }
+
+    @RequestMapping("/form_ids")
+    public List<FormId> getFormIds(@RequestParam("open-id") String openId) {
+        return sendMessJob.allFormIds(openId);
+    }
+
 
     @RequestMapping("")
     public String index() {
