72eb923b6c174f43a227e432f83bdf2e58e54e0f
jian.sun <jian.sun@vipshop.com>
2019-08-07 16:15:12 +0800

    _health_check response 1.0

--- src/main/java/com/aihuishou/bi/md/IndexC.java
+++ src/main/java/com/aihuishou/bi/md/IndexC.java
@@ -1,12 +1,14 @@
 package com.aihuishou.bi.md;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexC {
     @RequestMapping("_health_check")
-    public void healthCheck(){
-
+    public ResponseEntity healthCheck(){
+        return new ResponseEntity("1.0",HttpStatus.OK);
     }
 }
